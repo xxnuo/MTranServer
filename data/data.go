06cace9e25ef456b807d5d107429345628d5eb16
xxnuo <54252779+xxnuo@users.noreply.github.com>
2025-11-03 02:02:38 +0800

    feat: update

---
+++ data/data.go
@@ -0,0 +1,8 @@
+package bin
+
+import (
+	_ "embed"
+)
+
+//go:embed records.json
+var RecordsJson []byte
--- internal/models/const.go
+++
@@ -1,5 +0,0 @@
-package models
-
-const (
-	recordsUrl = "https://remote-settings.mozilla.org/v1/buckets/main/collections/translations-models/records"
-)
