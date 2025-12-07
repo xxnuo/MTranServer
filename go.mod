3ed87955eadeb1403ddf0a2755ae84d44b95512c
xxnuo <54252779+xxnuo@users.noreply.github.com>
2025-12-07 16:54:38 +0800

    feat: uncompress zstd support

--- go.mod
+++ go.mod
@@ -72,7 +72,7 @@ require (
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-version v1.6.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.15.11 // indirect
+	github.com/klauspost/compress v1.18.2 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- internal/utils/file.go
+++ internal/utils/file.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+
+	"github.com/klauspost/compress/zstd"
 )
 
 func VerifySHA256(filepath, expectedHash string) error {
@@ -41,4 +43,34 @@ func ComputeSHA256(filepath string) (string, error) {
 	}
 
 	return hex.EncodeToString(hash.Sum(nil)), nil
+}
+
+func DecompressZstd(src, dst string) error {
+	// Open the source file
+	inputFile, err := os.Open(src)
+	if err != nil {
+		return fmt.Errorf("failed to open source file: %w", err)
+	}
+	defer inputFile.Close()
+
+	// Create the decoder
+	decoder, err := zstd.NewReader(inputFile)
+	if err != nil {
+		return fmt.Errorf("failed to create zstd reader: %w", err)
+	}
+	defer decoder.Close()
+
+	// Create the destination file
+	outputFile, err := os.Create(dst)
+	if err != nil {
+		return fmt.Errorf("failed to create destination file: %w", err)
+	}
+	defer outputFile.Close()
+
+	// Copy the decompressed data
+	if _, err := io.Copy(outputFile, decoder); err != nil {
+		return fmt.Errorf("failed to decompress data: %w", err)
+	}
+
+	return nil
 }
