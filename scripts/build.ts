5dc64da99816ddcd30d861be1ee9e65b4d363e73
xxnuo <54252779+xxnuo@users.noreply.github.com>
2026-01-01 16:58:32 +0800

    Update build.ts

--- scripts/build.ts
+++ scripts/build.ts
@@ -9,13 +9,11 @@ const targets = [
   { bun: "bun-darwin-arm64", name: "darwin-arm64" },
   { bun: "bun-linux-x64", name: "linux-amd64" },
   { bun: "bun-linux-x64-baseline", name: "linux-amd64-legacy" },
-  { bun: "bun-linux-x64-modern", name: "linux-amd64-fast" },
   { bun: "bun-linux-arm64", name: "linux-arm64" },
   { bun: "bun-linux-x64-musl", name: "linux-amd64-musl" },
   { bun: "bun-linux-arm64-musl", name: "linux-arm64-musl" },
   { bun: "bun-windows-x64", name: "windows-amd64" },
-  { bun: "bun-windows-x64-baseline", name: "windows-amd64-legacy" },
-  { bun: "bun-windows-x64-modern", name: "windows-amd64-fast" },
+  { bun: "bun-windows-x64-baseline", name: "windows-amd64-legacy" }
 ];
 
 console.log("Cleaning dist...");
